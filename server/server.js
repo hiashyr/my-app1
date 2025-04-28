25f5237f22f01a688b8ebe8fba1617e2b4b66f29
hiashyr <shlimakovvvova@gmail.com>
2025-04-28 22:26:19 +0300

    Add_redux-saga

--- server/server.js
+++ server/server.js
@@ -8,6 +8,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(cors({
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 // Настройка Multer
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
